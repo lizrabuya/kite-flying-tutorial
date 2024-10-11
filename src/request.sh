163ea798918b839d157ad46de6e9e73d7a0e6c72
lizrabuya <115472349+lizrabuya@users.noreply.github.com>
2024-10-11 16:38:54 +1100

    test api to filter by meta_data

--- src/request.sh
+++ src/request.sh
@@ -2,4 +2,4 @@ set -e
 echo "Checking for running, failing, and blocked builds..."
 echo "Fetching the secret..."
 TOKEN=$1
-echo "$(curl -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -X GET 'https://api.buildkite.com/v2/organizations/lzrinc/pipelines/trigger-pipeline-action-integration-test/builds?state=running'  | jq '.[].web_url')"
+echo "$(curl -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -X GET 'https://api.buildkite.com/v2/organizations/lzrinc/pipelines/trigger-pipeline-action-integration-test/builds?state[]=running&meta_data[test]=dummy'  | jq '.[].web_url')"
